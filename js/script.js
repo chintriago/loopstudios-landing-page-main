d13245e32aa3b033029931fd80f15ba27c81c96c
chintriago <chrisintriago94@gmail.com>
2023-09-26 22:26:37 -0700

    added mobile pop up menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert("Javascript is running successfully!");
+let header = document.getElementById("header");
+let mobileMenu = document.getElementById("mobile-menu-2");
+let navBlack = document.getElementById("nav-black");
+let mobileMenuOpen = document.getElementById("mobile-menu-1");
+
+mobileMenu.addEventListener('click', () => {
+    navBlack.classList.remove("remove");
+    header.classList.add("remove");
+});
+
+mobileMenuOpen.addEventListener('click', () => {
+    navBlack.classList.add("remove");
+    header.classList.remove("remove");
+});
